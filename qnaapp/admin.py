c7b3c2b16c028120070a65ff82a92b252cff5bfb
Daniel-672 <kim672772@hotmail.com>
2020-09-05 20:26:46 +0900

    add 불필요 테이블삭제

--- qnaapp/admin.py
+++ qnaapp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from.models import Users, Qnaboard
-
-admin.site.register(Users)
-admin.site.register(Qnaboard)
+# from.models import Users, Qnaboard
+#
+# admin.site.register(Users)
+# admin.site.register(Qnaboard)
